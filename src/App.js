bf25a59434119e990a1a56946e86852505c2c66d
Florence Anipa <mawuaniipa@gmail.com>
2021-12-28 21:53:21 +0200

    Set up with eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
-}
+const App = () => {
+  return <div>Hello World</div>;
+};
 
 export default App;
